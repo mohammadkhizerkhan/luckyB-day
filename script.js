99ed9c6020cf48603d09f96d2dc0f977473a1af3
mohammadkhizerkhan <64767673+mohammadkhizerkhan@users.noreply.github.com>
2021-07-23 23:13:29 +0530

    designed css

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var btn=document.querySelector('#click')
 
 
-var inp=document.querySelector('#inTxt')
+var inp=document.querySelector('#inputTxt')
 var luck=document.querySelector('#lucky')
 
 var outputTxt=document.querySelector('#result')
@@ -10,13 +10,13 @@ var outputTxt=document.querySelector('#result')
 btn.addEventListener('click',function clickHandler(){
 	const inputval=((inp.value).split('-')).join("");
 	const luckval=luck.value;
+	console.log(inputval)
 	let sum=0;
 	for(letter of inputval){
 		sum=sum+Number(letter);
 	}
 	if(sum%luckval==0){
-		console.log(sum)
-		outputTxt.innerHTML=`sum is ${sum},your lucky number is ${luckval},and you are lucky`
+		outputTxt.innerHTML=`you are lucky`
 	}
 	else{
 		outputTxt.innerHTML="not lucky"
